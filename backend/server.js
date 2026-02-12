9f32e98aa69db0e3466469dcd277121f016537f2
obieh <o_amadic@yahoo.com>
2026-02-12 14:03:25 +0100

    ....

--- backend/server.js
+++ backend/server.js
@@ -14,4 +14,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-console.log(process.env);
+console.log(process.env.MONGO_URI);

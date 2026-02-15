d0ea356a62610b4d91b12443fb38a324fb2a9e6e
obieh <o_amadic@yahoo.com>
2026-02-15 01:56:10 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import Product from "./models/Product.js";
 
 dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/api/products", async (req, res) => {
   const products = req.body; // Placeholder for fetching products from the database
-  res.json(products);
+
   if (!products.name || !products.price || !products.imageUrl) {
     res.status(400).json({ message: "Missing required product fields" });
   }
-  const newproduct = new Product(product);
+  const newProduct = new Product(product);
   try {
-    await newproduct.save();
-    res.status(201).json(newproduct);
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
   } catch (error) {
     res.status(500).json({ message: error.message });
     console.error("Error saving product:", error.message);

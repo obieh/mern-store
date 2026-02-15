035ed849d77e3542005330685ab3b307feb21c03
obieh <o_amadic@yahoo.com>
2026-02-15 02:55:19 +0100

    test-post-product

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,23 +1,26 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     price: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: false,
     },
     imageUrl: {
-        type: String,
-        required: true
-    }
-}, { timestamps: true });
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
 
 const Product = mongoose.model("Product", productSchema);
 
--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import Product from "./models/Product.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.post("/api/products", async (req, res) => {
-  const products = req.body; // Placeholder for fetching products from the database
+app.post("/api/product", async (req, res) => {
+  const product = req.body; // Placeholder for fetching products from the database
 
-  if (!products.name || !products.price || !products.imageUrl) {
-    res.status(400).json({ message: "Missing required product fields" });
+  if (!product || !product.name || !product.price || !product.imageUrl) {
+    return res.status(400).json({ message: "Missing required product fields" });
   }
   const newProduct = new Product(product);
   try {
